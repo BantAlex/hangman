73ecb68b536142b043b6e92c49c6b4350a658733
BantAlex <bantalex3@gmail.com>
2024-06-24 20:35:45 +0300

    Add lose sate and more...

--- lib/game.rb
+++ lib/game.rb
@@ -2,10 +2,10 @@ require './lib/word_dictionary'
 
 class Game
 
-  attr_accessor :guesses, :word, :current_word, :letters, :choice
+  attr_accessor :guesses, :word, :current_word, :letters, :choice, :word_length
 
   def initialize
-    self.guesses = 15
+    @guesses = 15
     get_new_word
     puts "Weclome to hangman!"
     puts ""
@@ -13,27 +13,31 @@ class Game
     puts ""
     puts "The word will contain 5-12 letters."
     puts ""
+    puts "You have #{guesses}."
+    puts ""
     display_letters
-    get_choice
+    inclusion_check
   end
 
 
   def get_new_word
     @word = WordDictionary.new
-    @current_word = word.get_random_word
-    @letters = Array.new(current_word[0].length,"_ ")
+    @current_word = word.get_random_word[0]
+    @word_length = current_word.length
+    @letters = Array.new(@word_length,"_ ")
   end
 
   def display_letters
-    puts current_word #*this should be deleted when done
+    # puts current_word #*this should be deleted when done
     puts @letters.join
+    get_choice
   end
 
   def get_choice
     puts "Please choose a letter"
     @choice = gets.chomp.downcase
 
-    return @choice if ('a'..'z').include?(@choice) || ('A'..'Z').include?(@choice)
+    inclusion_check if ('a'..'z').include?(@choice) || ('A'..'Z').include?(@choice)
 
     puts ""
     puts "That's not a valid option."
@@ -42,7 +46,33 @@ class Game
   end
 
   def inclusion_check
+    if !@current_word.include?(@choice)
+      @guesses -= 1
+      out_of_guesses?
+      puts ""
+      puts "Nope!"
+      puts ""
+      puts "You have #{guesses} left."
+      puts ""
+      display_letters
+    else
+      put_choice_to_word
+    end
+  end
 
+  def put_choice_to_word
+    @current_word.each_char.with_index do |letter,i|
+      @letters[i] = choice if letter == choice
+    end
+    display_letters
+  end
+
+  def out_of_guesses?
+    if @guesses == 0
+      puts "You lost!"
+      puts "The word was '#{@current_word}'"
+      exit
+    end
   end
 
 end
